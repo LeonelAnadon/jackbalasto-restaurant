be82381626bc602909ba74c980d2d7c351517d6a
Leonel Anadon <leonelanadon95@gmail.com>
2024-07-20 12:01:38 -0300

    changes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,7 +4,7 @@ import ErrorPage from "../view/Error/ErrorPage";
 
 const router = createBrowserRouter([
   {
-    path: "/jackbalasto",
+    path: "/",
     element: <Home />,
     errorElement: <ErrorPage />,
   },
