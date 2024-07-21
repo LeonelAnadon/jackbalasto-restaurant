d2601ab4963d7b1a7b3cb0c99407234861908d58
Leonel Anadon <leonelanadon95@gmail.com>
2024-07-21 12:49:29 -0300

    c

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,13 +2,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../view/Home/Home";
 import ErrorPage from "../view/Error/ErrorPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: "/jackbalasto" }
+);
 
 const AppRouter = () => {
   return <RouterProvider router={router} />;
